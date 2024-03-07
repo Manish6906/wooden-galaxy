2f8006333e55b1e7ea736efb16c0f67467305171
manish-0911 <141442577+manish-0911@users.noreply.github.com>
2024-03-07 13:53:12 +0530

    anonymouds

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Ho</div>
+    <div className='flex'>
+      <div className='w-2/4'>
+        <img src="src\assets\loginImg.png" alt="" />
+      </div>
+      <div className='w-2/4 py-5'>
+        <div>
+          <h1 className='text-5xl font-bold py-4 text-[#444B59]'>WELCOME</h1>
+          <h3 className='text-base text-[#444B59] pb-{10px}'>Don't have a account <span className='text-base text-[#B5B0AB]'>Sign up</span> </h3>
+        </div>
+        <div className='pt-[50px]'>
+          <form>
+            <label>
+              Userame:
+            </label>
+            <input type="text" name="name" />
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
