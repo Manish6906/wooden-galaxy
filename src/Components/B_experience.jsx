b0075edf3a4037552274083cbf383e65dbc9f15c
bhumi2408 <bhumikagargsfd@gmail.com>
2024-03-07 12:25:47 +0530

    experiences added

--- src/Components/B_experience.jsx
+++ src/Components/B_experience.jsx
@@ -24,11 +24,11 @@ const B_experience = () => {
 
 
         <section className="my-6">
-            <div className='md:flex md:gap-20 md:justify-center'>
+            <div className='flex flex-col-reverse md:flex md:flex-row md:gap-20 md:justify-center'>
 
             <div className='text-center p-2 md:w-[400px] md:text-left md:mt-16'>
                   <h5 className=' italic my-2 text-[#E58411]'>MATERIALS</h5>
-                  <h2 className='text-[#1E1E1E] italic text-2xl'>Very Serious Materials For Making Furniture</h2>
+                  <h2 className='text-[#1E1E1E] italic text-2xl'>Very Serious Materials For <br/> Making Furniture</h2>
                   <p className='text-[#1E1E1E] text-sm opacity-70'>Because WG was very serious about designing furniture for our environment, using a very expensive and famous capital but at a relatively low price</p>
                   <a href="#" className='flex justify-center italic text-[#E58411]'>More info<FaLongArrowAltRight className='mt-[6px]'/></a>
               </div>
