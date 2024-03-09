4a32d95a9074f2aa8201baec5101856d41649320
rahul yadav <Rahul_203022@saitm.org>
2024-03-09 18:08:35 +0530

    Changes in App.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import B_experience from "../Components/B_experience";
+import Navbar from "../Components/Navbar";
 
 const Home = () => {
   return (
+    <>
+    <div className="">
+      <Navbar/>
+    </div>
     <div>
-    <B_experience />
-  </div>
-  )
-}
+      <B_experience />
+    </div>
+    </>
+  );
+};
 
 export default Home;
