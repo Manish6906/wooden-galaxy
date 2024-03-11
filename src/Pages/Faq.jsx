a886799fbbf60c8842373db418e63356ca0b9880
Rahul Yadav <rahul_203022@saitm.org>
2024-03-12 01:26:15 +0530

    12/3/2024 1:26AM

--- src/Pages/Faq.jsx
+++ src/Pages/Faq.jsx
@@ -67,18 +67,24 @@ const Faq = () => {
     <>
       <div>
         <div className="w-full h-50 flex justify-center items-center">
-          <img src={Nav} alt="" />
-          
+          <img src={Nav} alt="" className="w-screen" />
+
           <div className="absolute flex flex-col items-center justify-center">
-  <h1 className="font-bold text-3xl">Faq</h1>
-  <ul className="mt-5 flex gap-1">
-    <li><Link to="/" style={{ opacity: 1 }}>Home</Link></li>
-    <li style={{ opacity: 0.5 }}>></li>
-    <li><Link to="/Faq" style={{ opacity: 0.5 }}>Faq</Link></li>
-  </ul>
-</div>
-
-
+            <h1 className="font-bold text-3xl">Faq</h1>
+            <ul className="mt-5 flex gap-1">
+              <li>
+                <Link to="/" style={{ opacity: 1 }}>
+                  Home
+                </Link>
+              </li>
+              <li style={{ opacity: 0.5 }}>&gt;</li>
+              <li>
+                <Link to="/Faq" style={{ opacity: 0.5 }}>
+                  Faq
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
 
         <div className="flex justify-center mt-7 ">
